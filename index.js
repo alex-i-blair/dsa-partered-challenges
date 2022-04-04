686cd792cdf272db7df17834af192f5db24ebf4c
Alex Blair <bclan11@gmail.com>
2022-04-04 11:10:28 -0700

    evenish or oddish working

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ function reverseWords(sentence) {
     .map((word) => word.split('').reverse().join(''))
     .join(' ');
 }
-console.log(reverseWords(sent));
+// console.log(reverseWords(sent));
+
+function oddishOrEvenish(number) {
+  const reducer = (acc, curr) => acc + curr;
+  const numSum = number
+    .toString()
+    .split('')
+    .map((digit) => Number(digit))
+    .reduce(reducer);
+
+  return numSum % 2 === 0 ? 'Evenish' : 'Oddish';
+  // if (numSum % 2 === 0) {
+  //   return 'evenish';
+  // } else {
+  //   return 'oddish';
+  // }
+}
+
+const number = 122;
+console.log(oddishOrEvenish(number));
